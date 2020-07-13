14f86fce091dec03c56a5327d2adec2fec87bb0f
Praveen Sathelli <praveensathelli11@gmail.com>
2020-07-13 23:25:43 +0530

    added table & form roles,tab index, some code of color contrast

--- DOMValidation.js
+++ DOMValidation.js
@@ -1,5 +1,7 @@
 var domErrorElements = [];
 
+var tabindex = 0;
+
 function ValidateDOM(domElements) {
     domErrorElements = [];
 
@@ -15,11 +17,10 @@ function ValidateElement(domElement) {
             //console.log(domElement);
             return;
         }
-        console.log(domElement);
+        //console.log(domElement);
         var node = domElement.Element;
         var elementAttributes = node.getAttributeNames();
         var tagType = node.tagName.toLowerCase();
-
         switch (tagType) {
             case "html":
                 if (elementAttributes.indexOf("lang") < 0) {
@@ -30,6 +31,12 @@ function ValidateElement(domElement) {
             case "img":
                 if (elementAttributes.indexOf("alt") < 0) {
                     domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": "IMAGE Missing alt text", "ElementType": tagType });
+
+                }
+                break;
+            case "h1":
+                {
+                    validateColor(node);
                 }
                 break;
             case "label":
@@ -44,6 +51,27 @@ function ValidateElement(domElement) {
                 if (elementAttributes.indexOf("type") < 0) {
                     domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": "INPUT Element Missing type", "ElementType": tagType });
                 }
+                if (elementAttributes.indexOf("tabindex") < 0) {
+                    domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": "INPUT Element Missing tabindex", "ElementType": tagType });
+                }
+                else {
+                    if (tabindex + 1 == node.tabIndex) {
+                        tabindex = node.tabIndex;
+                    }
+                    else {
+                        domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": tagType + " element breaked tab index", "ElementType": tagType });
+                    }
+                }
+                break;
+            case "form":
+                if (elementAttributes.indexOf("role") < 0) {
+                    domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": "FORM Missing role attribute", "ElementType": tagType });
+                }
+                break;
+            case "table":
+                if (elementAttributes.indexOf("role") < 0) {
+                    domErrorElements.push({ "ElementXPath": domElement.ElementXPath, "ErrorDescription": "FORM Missing role attribute", "ElementType": tagType });
+                }
                 break;
             default:
                 break;
@@ -55,6 +83,34 @@ function ValidateElement(domElement) {
     }
 
 }
+
+function validateColor(element) {
+    console.log(window.getComputedStyle(element).color);
+    console.log(window.getComputedStyle(element).backgroundColor);
+    var rgb = window.getComputedStyle(element).backgroundColor.replace(/[^\d,]/g, '').split(',');
+    var rgbc = window.getComputedStyle(element).color.replace(/[^\d,]/g, '').split(',');
+    console.log(fullColorHex(rgb[0], rgb[1], rgb[2]));
+    var L1 = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+    var L2 = 0.2126 * rgbc[0] + 0.7152 * rgbc[1] + 0.0722 * rgbc[2];
+    console.log((L1 + 0.05) / (L2 + 0.05));
+}
+
+var rgbToHex = function (rgb) {
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+        hex = "0" + hex;
+    }
+    return hex;
+};
+
+var fullColorHex = function (r, g, b) {
+    var red = rgbToHex(r);
+    var green = rgbToHex(g);
+    var blue = rgbToHex(b);
+    return red + green + blue;
+};
+
+
 
 var domValidation = {
     "ValidateDOM": ValidateDOM
